76e439200abf11db0d39eeef7165fb5c9411a5ce
tkyg <80114433+tkyg@users.noreply.github.com>
2022-11-22 11:23:55 -0500

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,15 +1,23 @@
 class CheesesController < ApplicationController
 
+  def summary
+    "#{name}: $#{price}"
+  end
+
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: { error: 'Cheese not found' }, status: :not_found
+    end
   end
 
 end
